28b7fc097216972ae72475a913cadc165a7e1ef3
me <me>
2025-01-04 17:04:41 +0100

    Try to fix icon size

--- chadwm/config.def.h
+++ chadwm/config.def.h
@@ -37,7 +37,7 @@ static const int new_window_attach_on_end = 0; /*  1 means the new window will a
 #define ICONSIZE 19   /* icon size */
 #define ICONSPACING 8 /* space between icon and title */
 
-static const char *fonts[]          = {"Iosevka:style:medium:size=12" ,"JetBrainsMono Nerd Font Mono:style:medium:size=19" };
+static const char *fonts[]          = {"InconsolataGo Nerd Font:style:medium:size=12" };
 
 // theme
 #include "themes/onedark.h"
